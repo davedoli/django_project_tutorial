206b423c40289105c602c567f34bd67ce6e5b984
David Li <Li_Dong@bah.com>
2023-08-30 09:57:58 -0400

    finished the tutorial

--- django_project_tutorial/mysite/polls/views.py
+++ django_project_tutorial/mysite/polls/views.py
@@ -1,26 +1,41 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from .models import Question
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
 from django.urls import reverse
-
+from django.views import generic
+from django.utils import timezone
 from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    #return only questions that have some choices and not in the future
+
+    
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(choice__isnull=False, pub_date__lte=timezone.now()).distinct().order_by("-pub_date")[
+            :5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
